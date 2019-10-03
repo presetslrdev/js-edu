55ae0ecb7e2feb10daec2276ff816b8f71a8a9ac
Denis <presetslr@gmail.com>
2019-10-03 23:28:15 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
+const PRE_JUNIOR = 500;
+const JUNIOR = 800;
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+    return Math.ceil(knowsProgramming ? JUNIOR / config[focus] : (JUNIOR + PRE_JUNIOR) / config[focus] );
   };
-  
